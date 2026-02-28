e796f019210c5f7cd752b5710a4751790f445457
satlerkevin007-design <satlerkevin007@gmail.com>
2026-02-28 14:48:09 +0530

    Add files via upload

---
+++ kevinyy21.py
@@ -0,0 +1,9 @@
+# Take input from the user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Add the numbers
+sum = num1 + num2
+
+# Print the result
+print("The sum is:", sum)
